cad3ca7c4dabf24eec91d8d63490e1c155d9d77e
shawnmont772004 <21b47.shawn@sjec.ac.in>
2024-07-02 02:11:14 +0530

    new assets and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 
 import './App.css'
-
+import profilepic from './assets/shawn.jpg'
+import sunpic from './assets/sun.svg'
 function App() {
 
   return (
+    <>
     <h1 className="text-3xl font-space-grotesk">
-      Dan
-    </h1>
+    Dan</h1>
+    
+    <img src={profilepic} className="rounded-full object-cover w-64 h-64" alt="profile picture of shawn monteiro"/>
+    <img src={sunpic} className="rounded" alt="profile picture of shawn monteiro"/>
+    </>
+
   )
 }
 
