fe60b392bd0e13302afde0b17f744b417f51f15c
shawnmont772004 <21b47.shawn@sjec.ac.in>
2024-07-03 00:53:37 +0530

    icons n css

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@
 import './App.css'
 import profilepic from './assets/shawn.jpg'
 import sunpic from './assets/sun.svg'
+import githubIcon from './assets/github-light.svg'
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-space-grotesk">
-    Dan</h1>
+    <div className="font-space-grotesk">
+      <div>
+        <img src={profilepic} className="rounded-full  object-cover w-64 h-64" alt="profile picture of shawn monteiro"/>
+        <img src={sunpic} className="rounded " alt="profile picture of shawn monteiro"/>
+      </div>
+      <div>
+      <h1 className="text-3xl">Shawn<br/>Monteiro</h1>
+      <h2>Aspiring Software developer</h2>
+      <span>
+        <a href="https://github.com/shawnmont772004"><img src={githubIcon} alt="githubicon"/></a>
+      </span>
+    </div>
+    </div>
     
-    <img src={profilepic} className="rounded-full object-cover w-64 h-64" alt="profile picture of shawn monteiro"/>
-    <img src={sunpic} className="rounded" alt="profile picture of shawn monteiro"/>
     </>
 
   )
